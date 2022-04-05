20634bfcd873d09e6cd22e095e58f00c0b61a1f1
Adel <khusainovad@gmail.com>
2022-04-05 09:45:22 +0300

    + mapping name

--- src/main/kotlin/com/learnSpringFramework/demo/sample/enterprise/flow/web/Controller.kt
+++ src/main/kotlin/com/learnSpringFramework/demo/sample/enterprise/flow/web/Controller.kt
@@ -2,8 +2,9 @@ package com.learnSpringFramework.demo.sample.enterprise.flow.web
 
 import com.learnSpringFramework.demo.sample.enterprise.flow.business.BusinessService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Component
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 //Sending response in right format
@@ -16,7 +17,13 @@ class Controller {
     //"/sum" => 100
     @GetMapping("/sum")
     fun displaySum() = businessService.calculateSum()
+
+    @GetMapping("/username/{name}")
+    fun askQuestion(@PathVariable name: String):ResponseEntity<String> {
+        return ResponseEntity.ok("Hey $name, do you like banana pudding?")
+    }
 }
+
 
 
 
